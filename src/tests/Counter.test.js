05300ba55a6a82cc57aed39168fb907cecca2a2d
Aman Shaan <ashaan2019@fau.edu>
2024-11-21 11:25:06 -0500

    modification for announcment about render

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,29 +1,32 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Counter from '../components/Counter';
 
-beforeEach(() => {
-  render(<Counter />);
-})
-
 test('renders counter message', () => {
+  render(<Counter />);
+
   const counterMessage = screen.getByText(/Counter/i);
   expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
+  render(<Counter />);
+
   const countValue = screen.getByTestId('count');
   expect(countValue).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
+  render(<Counter />);
+
   const incrementButton = screen.getByText('+');
-  fireEvent.click(incrementButton);
-  
+  fireEvent.click(incrementButton)
   const countValue = screen.getByTestId('count');
   expect(countValue).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
+  render(<Counter />);
+
   const decrementButton = screen.getByText('-');
   fireEvent.click(decrementButton);
   const countValue = screen.getByTestId('count');
